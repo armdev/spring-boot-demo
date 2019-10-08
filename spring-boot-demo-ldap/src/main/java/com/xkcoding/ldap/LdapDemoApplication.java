f0342387782d596094fb05fa0ba01c8066868623
Armen Arzumanyan <armena@YLT190013.corp.optym.net>
2019-10-08 12:27:00 +0400

    just update

--- spring-boot-demo-ldap/src/main/java/com/xkcoding/ldap/LdapDemoApplication.java
+++ spring-boot-demo-ldap/src/main/java/com/xkcoding/ldap/LdapDemoApplication.java
@@ -13,6 +13,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LdapDemoApplication {
 
+    
+    ///
     public static void main(String[] args) {
         SpringApplication.run(LdapDemoApplication.class, args);
     }
